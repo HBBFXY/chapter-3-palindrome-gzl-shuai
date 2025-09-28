8fe30ea1e75bacdecf3a4bb3168f6a016bb140f4
gzl-shuai <1558507848@qq.com>
2025-09-28 10:40:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# 这里编写你的代码
+num_str = input("请输入一个5位数字: ")
+
+# 验证输入是否为5位数字
+if len(num_str) != 5 or not num_str.isdigit():
+    print("输入错误: 请输入5位数字")
+else:
+    # 判断是否为回文数
+    if num_str == num_str[::-1]:
+        print("是回文数")
+    else:
+        print("不是回文数")
